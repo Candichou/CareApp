eab0b06160f0d941ba75a07ca3347931835c0b67
/ <candice.chevret@gmail.com>
2026-02-26 17:25:56 +0100

    V1 css

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+interface Doctor {
+  id: number;
+  name: string;
+  firstname: string;
+  speciality: string;
+  street: string;
+  town: string;
+  postale_code: string;
+  tag: string;
+}
+
+export default function App() {
+  const [cardDoctor, setCardDoctor] = useState<Doctor[]>([]);
+  /*   const [...arrayDocteur, ...setArrayDocteur] = useState(""); */
+
+  useEffect(() => {
+    async function fetchDoctor() {
+      const response = await fetch("http://localhost:4242/doctor");
+      const dataDoctor = await response.json();
+      console.log(dataDoctor);
+      setCardDoctor(dataDoctor.response);
+      /*  setArrayDocteur(dataDoctor.results); */
+    }
+    fetchDoctor();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <article className="navbar">
+          <ul>
+            <li>
+              <h1>Aequia.</h1>
+            </li>
+            <li>
+              <a href="se connecter">Se connecter</a>
+            </li>
+          </ul>
+        </article>
+      </header>
+      <main>
+        <section className="search-zone">
+          <h2>Recherche ton spécialiste bienveillant</h2>
+          <input
+            type="text"
+            placeholder="cardiologue... ville... code postale..."
+          />
+          <button>rechercher</button>
+        </section>
+        <section className="display-doctor">
+          {cardDoctor.map((doctor) => {
+            return (
+              <div key={doctor.id}>
+                <ul className="card-doctor">
+                  <li>
+                    Dr {doctor.name} {doctor.firstname}
+                  </li>
+                  <li>{doctor.speciality}</li>
+                  <li>
+                    {doctor.street}, {doctor.town}
+                  </li>
+                  <li>{doctor.postale_code}</li>
+                </ul>
+                <ul className="card-tag">
+                  <li>{doctor.tag}</li>
+                </ul>
+              </div>
+            );
+          })}
+        </section>
+      </main>
+      <footer>
+        <section>
+          <article className="footer-brand">
+            <h2>Aequia.</h2>
+            <button>Me contacter</button>
+          </article>
+          <article className="footer-mention">
+            <h3>© 2026 Aequia</h3>
+          </article>
+        </section>
+      </footer>
     </>
-  )
+  );
 }
-
-export default App
